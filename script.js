62913cd3130ebcddf985820cf6edd45279dd2c4e
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-24 07:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function timing(){
+	let timer = document.getElementById("timer");
+	let time = new Date();
+	timer.textContent = time.toLocaleString();
+}
+setInterval(timing, 1000);
